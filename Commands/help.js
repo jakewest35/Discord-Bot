a8a7bf75b9f609784c9900dc888d7778a7256c0c
Jake West <jswest95@yahoo.com>
2020-03-25 23:18:17 -0700

    added additional commands

--- Commands/help.js
+++ Commands/help.js
@@ -2,7 +2,17 @@ module.exports = {
     name: 'help',
     description: "Lists all available commands",
     execute(message, args) {
-        message.channel.send('Available commands are: !hello, !user, !info version, !info author, !clear [number of messages you want to delete'
-        + '(no brackets)], !help');
+        if (args[1] === 'commands') {
+            message.channel.send('Available commands are: !hello, !user, !info version, !info author, !clear [number of messages you want to delete'
+            + '(no brackets)], !help');
+        }
+
+        else if (args[1] === 'info') {
+            message.channel.send('Available information is: version, author.')
+        }
+
+        else {
+            message.channel.send('Please specify what you would like help with.')
+        }
     }
 }
